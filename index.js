45b346c6928c73fdd4fee89b66913baef7c859d1
Andidev30 <aandi30082001@gmail.com>
2020-08-13 21:45:31 +0700

    setup routes and controller

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const port = 3008
 
 app.use(bodyParser.json())
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.use('/', (req, res) => {
+    res.send('hello-world')
+})
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
