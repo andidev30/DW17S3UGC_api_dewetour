7f8b0dfc089c2b98623876a5eba0858be36a737a
Andidev30 <aandi30082001@gmail.com>
2020-08-15 16:26:58 +0700

    add endpoint Login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,37 +1,47 @@
-const {User} = require('../models');
+const {
+    User
+} = require('../models');
 const bycript = require('bcrypt')
 const jwt = require('jsonwebtoken');
 const joi = require('@hapi/joi');
 
 exports.register = async (req, res) => {
     try {
-        const {fullName, email, password, phone, address} = req.body
+        const {
+            fullName,
+            email,
+            password,
+            phone,
+            address
+        } = req.body
 
         const schema = joi.object({
-            fullName : joi.string().min(2).required(),
-            email : joi.string().email().min(7).required(),
-            password : joi.string().min(8).required(),
+            fullName: joi.string().min(2).required(),
+            email: joi.string().email().min(7).required(),
+            password: joi.string().min(8).required(),
             phone: joi.string().min(11).required(),
             address: joi.string().min(10).required()
         })
 
-        const {error} = schema.validate(req.body)
+        const {
+            error
+        } = schema.validate(req.body)
 
         if (error) return res.status(400).send({
-            error : {
+            error: {
                 message: error.details[0].message
             }
         })
 
         const checkEmail = await User.findOne({
-            where : {
+            where: {
                 email
             }
         })
 
-        if(checkEmail) return res.status(400).send({
-            error : {
-                message : "user already exist"
+        if (checkEmail) return res.status(400).send({
+            error: {
+                message: "user already exist"
             }
         })
 
@@ -52,16 +62,79 @@ exports.register = async (req, res) => {
 
         res.status(200).send({
             message: "you have been registered",
-            data : {
+            data: {
                 email: data.email,
                 token
             }
         })
     } catch (error) {
-        res.status(200).send({
-            error : {
+        res.status(500).send({
+            error: {
                 message: "Internal server error"
             }
         })
     }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const {
+            email,
+            password
+        } = req.body
+    
+        const schema = joi.object({
+            email: joi.string().email().min(7).required(),
+            password: joi.string().required()
+        })
+    
+        const {
+            error
+        } = schema.validate(req.body)
+    
+        if (error) return res.status(400).send({
+            error: {
+                message: error.details[0].message
+            }
+        })
+
+        const checkEmail = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (!checkEmail) return res.status(400).send({
+            error: {
+                message: "email and password don't match"
+            }
+        })
+
+        const checkPassword = await bycript.compare(password, checkEmail.password)
+        if(!checkPassword) return res.status(400).send({
+            error : {
+                message : "email and password don't match"
+            }
+        })
+
+        const token = jwt.sign({
+            id: checkEmail.id
+        }, process.env.HASHPASSWORD)
+
+        res.status(200).send({
+            message: "login Success",
+            data: {
+                email,
+                token
+            }
+        })
+    } catch (error) {
+        res.status(500).send({
+            error: {
+                message: "Internal server error",
+                error
+            }
+        })
+    }
+
 }
